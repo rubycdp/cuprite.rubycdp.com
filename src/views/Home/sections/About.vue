8cc2be8fa69d04e165ec8360e14f8264cda0b359
Nikita Makarin <zn.makarin@gmail.com>
2020-04-21 16:51:27 +0300

    Remove Three section

--- src/views/Home/sections/About.vue
+++ src/views/Home/sections/About.vue
@@ -2,21 +2,18 @@
   <div class="about">
     <One class="about__block" />
     <Two class="about__block" />
-    <Three class="about__block" />
   </div>
 </template>
 
 <script>
 import One from '@/views/Home/sections/About/One'
 import Two from '@/views/Home/sections/About/Two'
-import Three from '@/views/Home/sections/About/Three'
 
 export default {
   name: 'About',
   components: {
     One,
-    Two,
-    Three
+    Two
   },
   data () {
     return {
